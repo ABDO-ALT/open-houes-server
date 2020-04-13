7fcb30817e886ae0009b04c5d77649443f275bbb
Abdo Mohamad <abdo@AiralkhasbAbdo.home>
2020-04-13 20:21:09 +0200

    I install nodemon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer(function(req, res) {
-  res.end("Hello World!");
+  res.end("Hello  Nandan, Said, Mouaz");
 });
 
 server.listen(5000);

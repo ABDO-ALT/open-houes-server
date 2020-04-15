73fd03f36f88771c90cb8a9e9486197b27d26527
Abdo Mohamad <abdo@AiralkhasbAbdo.home>
2020-04-15 13:00:00 +0200

    I create one colomn more to table database for age

--- database.sql
+++ database.sql
@@ -1,19 +1,26 @@
 -- To create now database  opject
-createdb open-house  - -u postgres
+createdb
+open-house  - -u postgres
 --To entey your database opject 
-qspl open-house - -u postgres
+qspl
+open-house - -u postgres
 --Whatch out delete space between tow dash at the letter (u) 
 
 -- For create a table  
-CREATE TABLE clients (
-    id        SERIAL PRIMARY KEY,
-    first_name      VARCHAR NOT NULL,
-    last_name      VARCHAR NOT NULL,
-    email     VARCHAR UNIQUE,
-    city      VARCHAR,
-    phone_number   VARCHAR UNIQUE,
-    user_type      VARCHAR NOT NULL,
-    gender    VARCHAR
+CREATE TABLE clients
+(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE,
+    city VARCHAR,
+    phone_number VARCHAR UNIQUE,
+    age   DATE,
+    user_type VARCHAR NOT NULL,
+    gender VARCHAR
 );
 -- To create informations use (INSERT) Whatch out use big letter
-INSERT INTO clients (first_name,last_name, email, city,phone_number,user_type,gender) VALUES ('said','belal','j.smith@johnsmith.org','barcelona','+34 671295766','guest','male');
+INSERT INTO clients
+    (first_name,last_name, email, city,phone_number,user_type,gender)
+VALUES
+    ('said', 'belal', 'j.smith@johnsmith.org', 'barcelona', '+34 671295766', 'guest', 'male');
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 const bodyParser = require("body-parser");
 const secret = require("./secret");
 
-
+app.use(bodyParser.json());
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -12,6 +12,7 @@ const pool = new Pool({
   password: secret.dbPassword,
   port: 5432,
 });
+
 app.get("/", function (req, res) {
   res.send("hello Mouaz, said, Nandan");
 });
@@ -30,22 +31,24 @@ function createNewuser(pool, req) {
   const newEmail = req.body.email;
   const newCity = req.body.city;
   const newPhone_number = req.body.phone_number;
+  const newAge = req.body.age;
   const newUser_type = req.body.user_type;
   const newGenber = req.body.gender;
   //first_name,last_name, email, city,phone_number,user_type,gender
   const query =
-    "INSERT INTO clients(first_name,last_name, email, city,phone_number,user_type,gender) VALUES ($1, $2, $3, $4,$5, $6 ,$7)";
+    "INSERT INTO clients(first_name,last_name, email, city,phone_number,age,user_type,gender) VALUES ($1, $2, $3, $4,$5, $6 ,$7,$8)";
   return pool.query(query, [
     newFirstName,
     newLast_name,
     newEmail,
     newCity,
     newPhone_number,
+    newAge,
     newUser_type,
     newGenber,
   ]);
 }
-app.use(bodyParser.json());
+
 app.post("/clients", function (req, res) {
   createNewuser(pool, req)
     .then(() => {
